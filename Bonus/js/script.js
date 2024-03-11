ebc7f4afe895481085df97b5144dd5a5e359afc3
DavideTessari <davtex@gmail.com>
2024-03-11 17:27:42 +0100

    bonus complete

---
+++ Bonus/js/script.js
@@ -0,0 +1,10 @@
+// Dato un array contenente 10 numeri, stampare in console il numero più grande.
+
+// Dato un array contenente 10 numeri.
+let numbers = [5, 12, 3, 8, 15, 7, 10, 2, 9, 6];
+
+// Trova il numero più grande nell'array.
+let biggerNumber = Math.max(...numbers);
+
+// Stampare il numero più grande in console.
+console.log("Il numero più grande nell'array è:", biggerNumber);
