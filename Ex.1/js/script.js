5c9b9ea63e308e49ff11ad693b68a82cdcb1ce60
DavideTessari <davtex@gmail.com>
2024-03-11 15:24:24 +0100

    ex.1 complete

--- Ex.1/js/script.js
+++ Ex.1/js/script.js
@@ -1,1 +1,15 @@
 // Chiedere all'utente 5 numeri. Inserire i numeri nell'array ma senza creare duplicati (inserire il numero nell'array solo se non è già presente).
+
+// Chiedi all'utente 5 numeri e salvali in un array
+let numbers = [];
+
+for (let i = 0; i < 5; i++) {
+  const userNumber = parseInt(prompt("Inserisci un numero:"));
+
+  // Inserire i numeri nell'array ma senza creare duplicati
+  if (!numbers.includes(userNumber)) {
+    numbers.push(userNumber);
+  }
+}
+
+alert(numbers);
