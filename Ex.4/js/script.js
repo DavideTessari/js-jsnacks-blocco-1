815c3e5a7a288861ee79a5be5e781cfcf9901ceb
DavideTessari <davtex@gmail.com>
2024-03-11 16:58:52 +0100

    ex.4 complete

--- Ex.4/js/script.js
+++ Ex.4/js/script.js
@@ -1,2 +1,17 @@
 // Calcola la somma e la media dei primi 10 numeri.
 
+// Calcola la somma dei primi 10 numeri.
+let somma = 0;
+for (let i = 1; i < 10; i++) {
+    somma += i;
+}
+
+// Calcola la media dei primi 10 numeri.
+let media = somma / 10;
+
+alert("La somma dei primi 10 numeri: " + somma);
+alert("La media dei primi 10 numeri: " + media);
+
+
+
+
