d6487f6bdd3bf373fc0fa82ac0d7a1a02ebe8c5e
DavideTessari <davtex@gmail.com>
2024-03-11 15:35:48 +0100

    complete ex.2

--- Ex.2/js/script.js
+++ Ex.2/js/script.js
@@ -1,2 +1,13 @@
 // Il software deve chiedere per 5 volte all’utente di inserire un numero.
 // Il programma stampa la somma di tutti i numeri inseriti.
+
+// Chiedi all'utente 5 numeri e salvali in un array
+let sum = 0;
+for (let i = 0; i < 5; i++) {
+  const userNumber = parseInt(prompt("Inserisci un numero:"));
+
+  // Stampa la somma di tutti i numeri
+  sum += userNumber;
+}
+
+alert(sum)
